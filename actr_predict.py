23d45f90b77b16015ca7c5cd057ded7961e6ef8b
Foo1 <390878830@qq.com>
2023-11-18 00:27:03 +0800

    change

--- actr_predict.py
+++ actr_predict.py
@@ -21,13 +21,17 @@ def PredictVelocity(v1,v2,v3):
 	return predict_v , execution_time
 
 
-def PredictLocation(x1 , y1 , x2 , y2 , x3 , y3 , v3 , predict_v):
-	
+def PredictLocationLine(x1 , y1 , x2 , y2 , x3 , y3 , v3 , predict_v):
+
 	start_time = time.time()
 
 	d_x = 0.1 * 0.5 * (predict_v + v3)
 	slope1 = (y2 - y1) / (x2 - x1)
 	slope2 = (y3 - y2) / (x3 - x2)
+	if (x1==x2 & y1==y2):
+		slope1 = float('inf')
+	if (x2==x3 & y2==y3):
+		slope2 = float('inf')
 	
 	d_e = math.sqrt(d_x ** 2 / (1 + slope2 ** 2))
 	d_n = slope1 * d_e
@@ -38,6 +42,44 @@ def PredictLocation(x1 , y1 , x2 , y2 , x3 , y3 , v3 , predict_v):
 		predict_y = y3 - d_n
 
 	end_time = time.time()
+
+	execution_time = end_time - start_time
+	return predict_x , predict_y, execution_time
+
+def PredictLocationCircle(x1 , y1 , x2 , y2 , x3 , y3 , v3 , predict_v):
+
+	start_time = time.time()
+
+	d_x = 0.1 * 0.5 * (predict_v + v3)
+	slope1 = (y2 - y1) / (x2 - x1)
+	slope2 = (y3 - y2) / (x3 - x2)
+
+	midpoint1 = ((x1 + x2) / 2, (y1 + y2) / 2)
+	midpoint2 = ((x2 + x3) / 2, (y2 + y3) / 2)
+	if (x1==x2 & y1==y2):
+		slope1 = float('inf')
+	if (x2==x3 & y2==y3):
+		slope2 = float('inf')
+
+	center_x = (midpoint2[1] - midpoint1[1] + slope1 * midpoint1[0] - slope2 * midpoint2[0]) / (slope1 - slope2)
+	center_y = slope1 * (center_x - midpoint1[0]) + midpoint1[1]
+
+	radius = math.sqrt((center_x - x1) ** 2 + (center_y - y1) ** 2)
+
+	theta = math.atan2(point3[1] - center_y, point3[0] - center_x)
+	
+	theta_new = theta + d_x / radius
+
+	predict_x = center_x + radius * math.cos(theta_new)
+	predict_y = center_y + radius * math.sin(theta_new)
+
+	if  ((predict_x - x2) ** 2 + (predict_y - y2) ** 2) < d_x * d_x :
+		theta_new = theta - d_x / radius
+		predict_x = center_x + radius * math.cos(theta_new)
+		predict_y = center_y + radius * math.sin(theta_new)
+
+	end_time = time.time()
+
 	execution_time = end_time - start_time
 	return predict_x , predict_y, execution_time
 
