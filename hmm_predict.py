9013dd3a1f5a6fd2bca3cd8cce9ca745932a82af
Foo1 <43083043+Foo1-Sov4H@users.noreply.github.com>
2023-12-22 16:47:35 +0800

    Add files via upload

--- hmm_predict.py
+++ hmm_predict.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from hmmlearn import hmm
 import numpy as np
-from collections import Counter
-
 
 df = pd.read_csv('training_dataset.csv')
 
@@ -13,63 +11,231 @@ data_dict = {}
 for group_name, group_data in grouped_data:
     data_dict[group_name] = group_data
 
-selected_groups_forward = grouped_data.filter(lambda x: x['laneId'].iloc[0] in [2, 3])
-#selected_groups_backward = grouped_data.filter(lambda x: x['laneId'].iloc[0] in [5, 6])
+selected_groups2 = grouped_data.filter(lambda x: x['laneId'].iloc[0] == 2)
+selected_groups3 = grouped_data.filter(lambda x: x['laneId'].iloc[0] == 3)
+selected_groups5 = grouped_data.filter(lambda x: x['laneId'].iloc[0] == 5)
+selected_groups6 = grouped_data.filter(lambda x: x['laneId'].iloc[0] == 6)
+
+train_data2 = selected_groups2[['x', 'y', 'xVelocity', 'yVelocity', 'state']].values
+train_data3 = selected_groups3[['x', 'y', 'xVelocity', 'yVelocity', 'state']].values
+train_data5 = selected_groups5[['x', 'y', 'xVelocity', 'yVelocity', 'state']].values
+train_data6 = selected_groups6[['x', 'y', 'xVelocity', 'yVelocity', 'state']].values
 
 
-train_data_forward = selected_groups_forward[['x', 'y', 'xVelocity', 'yVelocity', 'state']].values
-#train_data_backward = selected_groups_backward[['x', 'y', 'xVelocity', 'yVelocity', 'state']].values
+# HMM model for lane 2 & 3
 
+df = pd.read_csv('train_dataset2c.csv')
 
-#model_backward = hmm.GaussianHMM(n_components=6, covariance_type="full", n_iter=1000)
-model_forward = hmm.GaussianHMM(n_components=12, covariance_type="full", n_iter=1000)
+train_data2c = df[['x', 'y', 'xVelocity', 'yVelocity', 'state']].values
 
-#model_backward.fit(train_data_backward)
-model_forward.fit(train_data_forward)
+model2c = hmm.GaussianHMM(n_components=4, covariance_type="full", n_iter=10000)
+
+model2c.fit(train_data2c)
 
 print("Training Finish!")
+print(model2c.means_)
 
-### lane2  state 2
-### lane2->3 state 3
-### lane3  state 4
-current_data = [[162.75,9.39,-32.04,0.00,2],
-                [161.58,9.39,-32.06,0.00,2],
-                [160.36,9.39,-32.07,0.00,2]]
+# Test for Vehicle on lane 2
 
-for i in range(5):
-    predicted_states = model_forward.predict(current_data)
-    matrix = model_forward.means_
-    print(matrix)
-    last_column = matrix[:, -1].astype(int)
-    most_common_value = Counter(last_column).most_common(1)[0][0]
-    filtered_matrix = matrix[last_column == most_common_value]
-    vx_predict = np.mean(filtered_matrix[:, 2])
-    vy_predict = np.mean(filtered_matrix[:, 3])
-    x_predict = 0.04 * 0.5 * (vx_predict + current_data[2][2]) + current_data[2][0]
-    y_predict = 0.04 * 0.5 * (vy_predict + current_data[2][3]) + current_data[2][1]
-    print("Pridect Result:" , x_predict, y_predict, vx_predict, vy_predict, most_common_value)
-    current_data = [current_data[1], current_data[2],
-                    [x_predict, y_predict, vx_predict, vy_predict, most_common_value]]
-## 3 3 3 3 3 2 2 4 4 4 
-##Result:
-# hidden state mean:
-#[[ 2.02141354e+02  1.33109540e+01 -3.29552675e+01 -7.55215287e-03  4.00000000e+00]
-# [ 2.07710547e+02  9.80253077e+00 -3.31883583e+01 -2.79903778e-02  2.00000000e+00]
-# [ 2.02745861e+02  1.20681786e+01 -3.27832012e+01 -3.09041130e-02  3.00000000e+00]
-# [ 2.99652056e+02  9.01791648e+00 -2.43437672e+01  2.29993836e-02  3.00000000e+00]
-# [ 2.09923077e+02  9.44613928e+00 -2.60526823e+01 -3.77459548e-03  2.00000000e+00]
-# [ 8.31976240e+01  8.98230760e+00 -2.46113813e+01 -2.76846841e-02  3.00000000e+00]]
+right = [0, 0, 0, 0, 0]
+error = [0, 0, 0, 0, 0]
+for i in range(1000):
+    # 'x', 'y', 'xVelocity', 'yVelocity', 'state'
+    current_data = [train_data2[i+2]]
+    history_data1 = [train_data2[i]]
+    history_data2 = [train_data2[i+1]]
+    #print(history_data1,history_data2,current_data,)
+    for j in range(5):        
+        predicted_state = model2c.predict(current_data)
+        #print('predict state is:',predicted_state)
+        posterior_probs = model2c.predict_proba(current_data)
+        most_probable_state = np.argmax(posterior_probs)
+        predicted_observation = model2c.means_[most_probable_state]
+        if (predicted_state == [3]):
+            predict_result = current_data[0][4] 
+        else:
+            predict_result = predicted_observation[4]
+        if(predict_result==int(train_data2[i+j+3][4])):
+            right[j] = right[j] + 1
+        else:
+            error[j] = error[j] + 1
+        
+        next_vx = current_data[0][2] + ((current_data[0][2] - history_data2[0][2]) - (history_data2[0][2] - history_data1[0][2]))
+        next_vy = current_data[0][3] + ((current_data[0][3] - history_data2[0][3]) - (history_data2[0][3] - history_data1[0][3]))
+        next_x = 0.04 * 0.5 * (next_vx + current_data[0][2]) + current_data[0][0]
+        next_y = 0.04 * 0.5 * (next_vy + current_data[0][3]) + current_data[0][1]
+
+        current_data = [[next_x, next_y, next_vx, next_vy, predict_result]]
+        history_data1 = history_data2
+        history_data2 = current_data
+        print('Predict result is: ', current_data)
+        print('Real result is:', train_data2[i+j+3])
+        
+print('Right Num:', right, 'Error Num:', error)
+
+# Test for Vehicle on lane 3
+
+right = [0, 0, 0, 0, 0]
+error = [0, 0, 0, 0, 0]
+for i in range(1000):
+    # 'x', 'y', 'xVelocity', 'yVelocity', 'state'
+    current_data = [train_data3[i+2]]
+    history_data1 = [train_data3[i]]
+    history_data2 = [train_data3[i+1]]
+    #print(history_data1,history_data2,current_data,)
+    for j in range(5):        
+        predicted_state = model2c.predict(current_data)
+        #print('predict state is:',predicted_state)
+        posterior_probs = model2c.predict_proba(current_data)
+        most_probable_state = np.argmax(posterior_probs)
+        predicted_observation = model2c.means_[most_probable_state]
+        if (predicted_state == [3]):
+            predict_result = current_data[0][4] 
+        else:
+            predict_result = predicted_observation[4]
+        if(predict_result==int(train_data3[i+j+3][4])):
+            right[j] = right[j] + 1
+        else:
+            error[j] = error[j] + 1
+        
+        next_vx = current_data[0][2] + ((current_data[0][2] - history_data2[0][2]) - (history_data2[0][2] - history_data1[0][2]))
+        next_vy = current_data[0][3] + ((current_data[0][3] - history_data2[0][3]) - (history_data2[0][3] - history_data1[0][3]))
+        next_x = 0.04 * 0.5 * (next_vx + current_data[0][2]) + current_data[0][0]
+        next_y = 0.04 * 0.5 * (next_vy + current_data[0][3]) + current_data[0][1]
+
+        current_data = [[next_x, next_y, next_vx, next_vy, predict_result]]
+        history_data1 = history_data2
+        history_data2 = current_data
+        print('Predict result is: ', current_data)
+        print('Real result is:', train_data3[i+j+3])
+
+print('Right Num:', right, 'Error Num:', error)
+# Right Num: [998, 996, 994, 992, 990] Error Num: [2, 4, 6, 8, 10]
 
 
+# HMM model for lane 5 & 6
 
 
-# Want to predict the results after 0.8s
-# Our dataset is 0.04s per frame 
+df = pd.read_csv('train_dataset6c.csv')
 
-##### method 1:
-######### The prediction result after 0.8s is obtained by iterating 20 times.
-######### Problem: The vehicle's position has been obtained during this iterative process, and there is a considerable error.
+train_data6c = df[['x', 'y', 'xVelocity', 'yVelocity', 'state']].values
+print(train_data6c)
+model6c = hmm.GaussianHMM(n_components=4, covariance_type="full", n_iter=10000)
 
-##### method 2:
-######### The dataset to training can be changed to (pick i = 20 x j + k) to train 
-######### Problem: data is not enough, some data group will only have 5 data.
+model6c.fit(train_data6c)
+
+print("Training Finish!")
+print(model6c.means_)
+
+
+right = [0, 0, 0, 0, 0]
+error = [0, 0, 0, 0, 0]
+for i in range(1000):
+    # 'x', 'y', 'xVelocity', 'yVelocity', 'state'
+    current_data = [train_data3[i+2]]
+    history_data1 = [train_data3[i]]
+    history_data2 = [train_data3[i+1]]
+    #print(history_data1,history_data2,current_data,)
+    for j in range(5):        
+        predicted_state = model2c.predict(current_data)
+        #print('predict state is:',predicted_state)
+        posterior_probs = model2c.predict_proba(current_data)
+        most_probable_state = np.argmax(posterior_probs)
+        predicted_observation = model2c.means_[most_probable_state]
+        if (predicted_state == [3]):
+            predict_result = current_data[0][4] 
+        else:
+            predict_result = predicted_observation[4]
+        if(predict_result==int(train_data3[i+j+3][4])):
+            right[j] = right[j] + 1
+        else:
+            error[j] = error[j] + 1
+        
+        next_vx = current_data[0][2] + ((current_data[0][2] - history_data2[0][2]) - (history_data2[0][2] - history_data1[0][2]))
+        next_vy = current_data[0][3] + ((current_data[0][3] - history_data2[0][3]) - (history_data2[0][3] - history_data1[0][3]))
+        next_x = 0.04 * 0.5 * (next_vx + current_data[0][2]) + current_data[0][0]
+        next_y = 0.04 * 0.5 * (next_vy + current_data[0][3]) + current_data[0][1]
+
+        current_data = [[next_x, next_y, next_vx, next_vy, predict_result]]
+        history_data1 = history_data2
+        history_data2 = current_data
+        print('Predict result is: ', current_data)
+        print('Real result is:', train_data3[i+j+3])
+
+        
+print('Right Num:', right, 'Error Num:', error)
+
+
+right = [0, 0, 0, 0, 0]
+error = [0, 0, 0, 0, 0]
+for i in range(1000):
+    # 'x', 'y', 'xVelocity', 'yVelocity', 'state'
+    current_data = [train_data5[i+2]]
+    history_data1 = [train_data5[i]]
+    history_data2 = [train_data5[i+1]]
+    #print(history_data1,history_data2,current_data,)
+    for j in range(5):        
+        predicted_state = model6c.predict(current_data)
+        #print('predict state is:',predicted_state)
+        posterior_probs = model6c.predict_proba(current_data)
+        most_probable_state = np.argmax(posterior_probs)
+        predicted_observation = model6c.means_[most_probable_state]
+        if (predicted_state == [2]):
+            predict_result = current_data[0][4] 
+        else:
+            predict_result = predicted_observation[4]
+        if(predict_result==int(train_data5[i+j+3][4])):
+            right[j] = right[j] + 1
+        else:
+            error[j] = error[j] + 1
+        
+        next_vx = current_data[0][2] + ((current_data[0][2] - history_data2[0][2]) - (history_data2[0][2] - history_data1[0][2]))
+        next_vy = current_data[0][3] + ((current_data[0][3] - history_data2[0][3]) - (history_data2[0][3] - history_data1[0][3]))
+        next_x = 0.04 * 0.5 * (next_vx + current_data[0][2]) + current_data[0][0]
+        next_y = 0.04 * 0.5 * (next_vy + current_data[0][3]) + current_data[0][1]
+
+        current_data = [[next_x, next_y, next_vx, next_vy, predict_result]]
+        history_data1 = history_data2
+        history_data2 = current_data
+        print('Predict result is: ', current_data)
+        print('Real result is:', train_data5[i+j+3])
+
+        
+print('Right Num:', right, 'Error Num:', error)
+
+
+right = [0, 0, 0, 0, 0]
+error = [0, 0, 0, 0, 0]
+for i in range(1000):
+    # 'x', 'y', 'xVelocity', 'yVelocity', 'state'
+    current_data = [train_data6[i+2]]
+    history_data1 = [train_data6[i]]
+    history_data2 = [train_data6[i+1]]
+    for j in range(5):        
+        predicted_state = model6c.predict(current_data)
+        #print('predict state is:',predicted_state)
+        posterior_probs = model6c.predict_proba(current_data)
+        most_probable_state = np.argmax(posterior_probs)
+        predicted_observation = model6c.means_[most_probable_state]
+        if (predicted_state == [2]):
+            predict_result = current_data[0][4] 
+        else:
+            predict_result = predicted_observation[4]
+        if(predict_result==int(train_data6[i+j+3][4])):
+            right[j] = right[j] + 1
+        else:
+            error[j] = error[j] + 1
+        
+        next_vx = current_data[0][2] + ((current_data[0][2] - history_data2[0][2]) - (history_data2[0][2] - history_data1[0][2]))
+        next_vy = current_data[0][3] + ((current_data[0][3] - history_data2[0][3]) - (history_data2[0][3] - history_data1[0][3]))
+        next_x = 0.04 * 0.5 * (next_vx + current_data[0][2]) + current_data[0][0]
+        next_y = 0.04 * 0.5 * (next_vy + current_data[0][3]) + current_data[0][1]
+
+        current_data = [[next_x, next_y, next_vx, next_vy, predict_result]]
+        history_data1 = history_data2
+        history_data2 = current_data
+        print('Predict result is: ', current_data)
+        print('Real result is:', train_data6[i+j+3])
+
+        
+print('Right Num:', right, 'Error Num:', error)
